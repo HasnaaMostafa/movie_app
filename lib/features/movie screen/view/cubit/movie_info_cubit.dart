f383a77069f4d020b104b0d889cbce49f8165714
nadersakr <nader150656@eng.psu.edu.eg>
2024-09-01 03:20:52 +0300

    feat: replace similar with category into similar with the movie

--- lib/features/movie screen/view/cubit/movie_info_cubit.dart
+++ lib/features/movie screen/view/cubit/movie_info_cubit.dart
@@ -6,11 +6,11 @@ import 'package:movie_app/features/movie%20screen/view/cubit/movie_info_state.da
 class MovieInfoCubit extends Cubit<MovieInfoState> {
   MovieInfoCubit() : super(const MovieInfoInitial());
 
-  Future<void> loadMoviesByCategory(String category) async {
+  Future<void> loadSimilarMovies(int movieId) async {
     try {
       emit(const MovieInfoLoading());
       print("1");
-      List<Movie> movies = await MovieRepository.getMoviesByCategory(category);
+      List<Movie> movies = await MovieRepository.getSimilarMovies(movieId);
       print("2");
       emit(MovieInfoLoaded(movies));
       print("3");
