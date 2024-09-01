757c966e1d459e418207d547b7aa4623dd6ab09a
nadersakr <nader150656@eng.psu.edu.eg>
2024-09-01 03:13:27 +0300

    chore: Update API key in api_constants.dart

--- lib/core/constant/api_constants.dart
+++ lib/core/constant/api_constants.dart
@@ -1,5 +1,7 @@
 String baseUrl = "https://api.themoviedb.org/";
 String bearer = "Bearer ";
-String apiKey = "f40f1fb325c12c2fbfcc257fdcf158f9 ";
-
+String apiKey = "f40f1fb325c12c2fbfcc257fdcf158f9";
 //https://api.themoviedb.org/3/movie/popular?api_key=f40f1fb325c12c2fbfcc257fdcf158f9
+///
+// b1dcd7ea37508ddb34099a26032083b5
+///
